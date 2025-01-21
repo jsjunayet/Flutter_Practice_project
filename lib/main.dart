dd898f8fe92358d704e78a0cfbbf092f3e33c4d6
Junayet Shiblu <130874714+jsjunayet@users.noreply.github.com>
2025-01-21 23:36:12 +0600

    Floating action button and Snacker

--- lib/main.dart
+++ lib/main.dart
@@ -20,15 +20,38 @@ class MyApp extends StatelessWidget{
 }
 class HomeActivity extends StatelessWidget{
   const HomeActivity({super.key});
+  MySnackBar(message,context){
+    return ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(message))
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
    return Scaffold(
-     appBar: AppBar(title: Text("MY app"),
+     appBar: AppBar(title: Text("MY app", style: TextStyle(
+       color: Colors.white, // Text color set to white
+       fontSize: 20, // Optional: Font size
+       fontWeight: FontWeight.bold, // Optional: Font weight
+     ),),
        backgroundColor: Colors.indigo,
-       elevation: 5.0,
-       centerTitle: true,
+       titleSpacing: 20,
+       elevation: 50,
+       actions: [
+         IconButton(onPressed: (){MySnackBar('this is email',context);}, icon: Icon(Icons.email,color: Colors.white)),
+         IconButton(onPressed: (){MySnackBar("this is number", context);}, icon: Icon(Icons.numbers,color: Colors.white)),
+         IconButton(onPressed: (){MySnackBar("this is Setting", context);}, icon: Icon(Icons.settings,color: Colors.white)),
+         IconButton(onPressed: (){MySnackBar("this is Search", context);}, icon: Icon(Icons.search, color: Colors.white, size: 30,))
+       ],
 
+     ),
+     floatingActionButton: FloatingActionButton(
+       elevation: 20,
+       backgroundColor: Colors.indigo,
+       child: Icon(Icons.add, color: Colors.white,),
+       onPressed: (){
+         MySnackBar("this is added botton", context);
+       },
      ),
      body: Text("hello wrold"),
    );
